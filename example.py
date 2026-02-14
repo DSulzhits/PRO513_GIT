5b432d2678a88a0176ed9e5d7f79a5fdbca702fc
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2026-02-14 13:47:39 +0300

    Add func divide

--- example.py
+++ example.py
@@ -10,7 +10,12 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    return a / b
+
+
 if __name__ == '__main__':
     print(add(3, 5))
     print(subtract(5, 3))
     print(multiply(3, 5))
+    print(divide(15, 3))
